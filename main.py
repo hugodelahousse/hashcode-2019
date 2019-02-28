0271fbd1487d8350b7ea67cc675f8ddcdcfee8ea
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-02-28 20:22:59 +0100

    Add chunk splitting and distance calcultations

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import fileinput
+from pprint import pprint
+from tqdm import tqdm
+import numpy as np
+from random import shuffle
+
 
 def parse():
     lines = [line.strip() for line in fileinput.input()]
@@ -16,7 +21,7 @@ def parse():
                 set(map(tag_transform, data[2:])),
             )
         )
-    print(photos)
+    return photos
 
 
 
@@ -36,16 +41,63 @@ def tag_transform(tag):
     if value is not None:
         return value
     value = len(TAGS)
-    TAGS[value] = tag
+    TAGS[tag] = value
     return value
 
 def tags_distance(a, b):
+    inter = len(a & b)
     return min(
-        len(a & b),
-        len(a - b),
-        len(b - a),
+        inter,
+        len(a) - inter,
+        len(b) - inter,
     )
 
 
+
+class Distances:
+    distances = {}
+
+    def add_distance(self, i, j, d):
+        if not i < j:
+            return self.add_distance(j, i, d)
+        if not d:
+            return
+        self.distances[(i, j)] = d
+
+    def get_distance(self, i, j):
+        if not i < j:
+            return self.get_distance(j, i)
+        return self.distances.get((i, j))
+
+
+def compute_distances(images):
+    distances = Distances()
+    for i, a in enumerate(images):
+        for j, b in enumerate(images[i + 1:], start=i + 1):
+            distance = tags_distance(a.tags, b.tags)
+            distances.add_distance(i, j, distance)
+
+    return distances
+
+def main():
+    chunk_count = 20
+
+    images = parse()
+    # shuffle(images)
+    chunk_size = len(images) // chunk_count
+
+    print(chunk_count, chunk_size)
+    chunks = [images[i: i + chunk_size] for i in range(len(images), step=chunk_size)]
+
+
+    print('Computing distances')
+    for chunk in tqdm(chunks):
+        distances = compute_distances(chunk)
+        # pprint(distances.distances)
+
+    # m = get_max(distances)
+    # print(distances[m])
+
+
 if __name__ == "__main__":
-    parse()
+    main()
